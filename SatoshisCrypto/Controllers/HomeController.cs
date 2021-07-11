90039c8c8871e967a5f8734283292a44c1a19799
Faisal Rana <inquisitive@gmail.com>
2021-07-11 11:19:17 -0700

    got mysql database working

--- SatoshisCrypto/Controllers/HomeController.cs
+++ SatoshisCrypto/Controllers/HomeController.cs
@@ -1,25 +1,32 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using SatoshisCrypto.Models;
 
 namespace SatoshisCrypto.Controllers
 {
   public class HomeController : Controller
+  {
+    private readonly SatoshisCryptoContext _db;
+    public HomeController(SatoshisCryptoContext db)
     {
-      private readonly SatoshisCryptoContext _db;
-      public HomeController(SatoshisCryptoContext db) 
-      {
-        _db = db;
-      }
-
-      public IActionResult Index()
-        {
-            var allComments = Comment.GetComments();
-            
-            // _db.Comments.Add("hello world");
-            
-            return View(allComments);
-            
-        }
+      _db = db;
     }
+
+    public IActionResult Index()
+    {
+
+      var allComments = Comment.GetComments();
+      // var get8hrComments = Comment.GetComments4hr();
+      // var get4hrComments = Comment.GetComments8hr();
+      // var allComments = get8hrComments.Concat(get4hrComments).ToList();
+
+
+      _db.Comments.AddRange(allComments);
+      _db.SaveChanges();
+
+      return View(allComments);
+
+    }
+  }
 }
